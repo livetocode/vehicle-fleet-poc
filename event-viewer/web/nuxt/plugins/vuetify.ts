9cfd0e1eab6854f9132088a8a9837e3d02be03ff
Morgan Martinet <morgan.martinet@montreal.ca>
2024-09-15 21:05:47 -0400

    add material design icons font

--- event-viewer/web/nuxt/plugins/vuetify.ts
+++ event-viewer/web/nuxt/plugins/vuetify.ts
@@ -1,12 +1,19 @@
 // import this after install `@mdi/font` package
 import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })
