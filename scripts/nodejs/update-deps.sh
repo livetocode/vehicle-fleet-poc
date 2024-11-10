a76411fc611ec3e74ce9efcb6f4b9ddfbe1bd94f
Morgan Martinet <morgan.martinet@montreal.ca>
2024-11-10 09:57:11 -0500

    upgrade deps

--- scripts/nodejs/update-deps.sh
+++ scripts/nodejs/update-deps.sh
@@ -3,5 +3,5 @@ set -e
 (cd shared/javascript/messaging-lib && npm run update-deps)
 (cd event-collector/nodejs && npm run update-deps) &
 (cd event-generator/nodejs && npm run update-deps)
-(cd event-viewer/nodejs && npm run update-deps)
-(cd event-querier/web/nuxt && npm run update-deps)
+(cd event-querier/nodejs && npm run update-deps)
+(cd event-viewer/web/nuxt && npm run update-deps)
