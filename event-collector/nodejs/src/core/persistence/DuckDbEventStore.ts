13fc8311c567604cef9fdf9adf1c78e2adfb2898
Morgan Martinet <morgan.martinet@montreal.ca>
2025-04-22 13:02:43 -0400

    disable duckdb since it brings a 120MB package

--- event-collector/nodejs/src/core/persistence/DuckDbEventStore.ts
+++ event-collector/nodejs/src/core/persistence/DuckDbEventStore.ts
@@ -1,64 +1,64 @@
-import { Database, Statement } from "duckdb-async";
-import { EventStore, StoredEvent } from "./EventStore.js";
-import { TimeRange } from "core-lib";
+// import { Database, Statement } from "duckdb-async";
+// import { EventStore, StoredEvent } from "./EventStore.js";
+// import { TimeRange } from "core-lib";
 
-export class DuckDbEventStore<T> implements EventStore<T> {
-    private _db?: Database;
-    private _insertStatement?: Statement;
-    private _deleteStatement?: Statement;
-    private _selectStatement?: Statement;
+// export class DuckDbEventStore<T> implements EventStore<T> {
+//     private _db?: Database;
+//     private _insertStatement?: Statement;
+//     private _deleteStatement?: Statement;
+//     private _selectStatement?: Statement;
 
-    async init(): Promise<void> {
-        const db = await Database.create(":memory:");
-        this._db = db;
-        await db.run(`CREATE TABLE events (
-            timestamp TIMESTAMP,
-            partitionKey VARCHAR,
-            collectorIndex INTEGER,
-            event JSON
-        )`);
-        await db.run('CREATE INDEX events_idx ON events (timestamp);');
-        this._insertStatement = await db.prepare(`INSERT INTO events (timestamp, partitionKey, collectorIndex, event) VALUES (?, ?, ?, ?)`);
-        this._deleteStatement = await db.prepare(`DELETE FROM events WHERE timestamp >= ?::TIMESTAMP AND timestamp < ?::TIMESTAMP and collectorIndex = ?::INTEGER`);
-        this._selectStatement = await db.prepare(`
-            SELECT timestamp, partitionKey, collectorIndex, event 
-            FROM events 
-            WHERE collectorIndex = ?::INTEGER 
-            ORDER BY timestamp
-            OFFSET ?::INTEGER 
-            LIMIT 10000
-        `);
-    }
+//     async init(): Promise<void> {
+//         const db = await Database.create(":memory:");
+//         this._db = db;
+//         await db.run(`CREATE TABLE events (
+//             timestamp TIMESTAMP,
+//             partitionKey VARCHAR,
+//             collectorIndex INTEGER,
+//             event JSON
+//         )`);
+//         await db.run('CREATE INDEX events_idx ON events (timestamp);');
+//         this._insertStatement = await db.prepare(`INSERT INTO events (timestamp, partitionKey, collectorIndex, event) VALUES (?, ?, ?, ?)`);
+//         this._deleteStatement = await db.prepare(`DELETE FROM events WHERE timestamp >= ?::TIMESTAMP AND timestamp < ?::TIMESTAMP and collectorIndex = ?::INTEGER`);
+//         this._selectStatement = await db.prepare(`
+//             SELECT timestamp, partitionKey, collectorIndex, event 
+//             FROM events 
+//             WHERE collectorIndex = ?::INTEGER 
+//             ORDER BY timestamp
+//             OFFSET ?::INTEGER 
+//             LIMIT 10000
+//         `);
+//     }
 
-    async write(event: StoredEvent<T>): Promise<void> {
-        await this._insertStatement?.run(event.timestamp, event.partitionKey, event.collectorIndex, JSON.stringify(event.event));
-        await this._insertStatement?.finalize();
-    }
+//     async write(event: StoredEvent<T>): Promise<void> {
+//         await this._insertStatement?.run(event.timestamp, event.partitionKey, event.collectorIndex, JSON.stringify(event.event));
+//         await this._insertStatement?.finalize();
+//     }
 
-    async *fetch(collectorIndex?: number): AsyncGenerator<StoredEvent<T>[], any, unknown> {
-        if (this._selectStatement) {
-            let offset = 0;
-            while(true) {
-                const data = await this._selectStatement?.all(collectorIndex ?? 0, offset);
-                if (data.length > 0) {
-                    yield data.map(row => (
-                        {
-                            timestamp: row.timestamp,
-                            partitionKey: row.partitionKey,
-                            collectorIndex: row.collectorIndex,
-                            event: JSON.parse(row.event),
-                        }
-                    ));
-                    offset += data.length;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
+//     async *fetch(collectorIndex?: number): AsyncGenerator<StoredEvent<T>[], any, unknown> {
+//         if (this._selectStatement) {
+//             let offset = 0;
+//             while(true) {
+//                 const data = await this._selectStatement?.all(collectorIndex ?? 0, offset);
+//                 if (data.length > 0) {
+//                     yield data.map(row => (
+//                         {
+//                             timestamp: row.timestamp,
+//                             partitionKey: row.partitionKey,
+//                             collectorIndex: row.collectorIndex,
+//                             event: JSON.parse(row.event),
+//                         }
+//                     ));
+//                     offset += data.length;
+//                 } else {
+//                     break;
+//                 }
+//             }
+//         }
+//     }
 
-    async delete(range: TimeRange, collectorIndex?: number): Promise<void> {
-        await this._deleteStatement?.run(range.fromTime, range.untilTime, collectorIndex ?? 0);
-        await this._deleteStatement?.finalize();
-    }
-}
+//     async delete(range: TimeRange, collectorIndex?: number): Promise<void> {
+//         await this._deleteStatement?.run(range.fromTime, range.untilTime, collectorIndex ?? 0);
+//         await this._deleteStatement?.finalize();
+//     }
+// }
