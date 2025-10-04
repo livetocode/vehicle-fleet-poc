750511387919610ca891c5552f376adad8de848d
Morgan Martinet <morgan.martinet@montreal.ca>
2025-10-04 12:07:27 -0400

    code formatting

--- shared/javascript/data-lib/src/factories.ts
+++ shared/javascript/data-lib/src/factories.ts
@@ -4,15 +4,15 @@ import { AzureBlobDataframeRepository } from "./repositories/dataframe/AzureBlob
 
 export function createDataFrameRepository(config: OutputConfig) {
     if (config.storage.type === 'file') {
-        return new FileDataframeRepository(config.storage.folder);
+      return new FileDataframeRepository(config.storage.folder);
     }
     if (config.storage.type === 'azure-blob') {
-        const connectionString = process.env.VEHICLES_AZURE_STORAGE_CONNECTION_STRING || config.storage.connectionString;
+      const connectionString = process.env.VEHICLES_AZURE_STORAGE_CONNECTION_STRING || config.storage.connectionString;
       
       if (!connectionString) {
         throw Error('Azure Storage Connection string not found: VEHICLES_AZURE_STORAGE_CONNECTION_STRING');
       }
-        return new AzureBlobDataframeRepository(connectionString, config.storage.containerName);
+      return new AzureBlobDataframeRepository(connectionString, config.storage.containerName);
     }
     throw new Error(`Unknown output type "${config.storage.type}"`);
 }
